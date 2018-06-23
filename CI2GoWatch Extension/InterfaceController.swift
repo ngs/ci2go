9fb1f3c3ded770e151479bf9892bc7b86ccc6f26
Atsushi Nagase <a@ngs.io>
2018-06-23 09:35:10 +0900

    Add Watch App target

---
+++ CI2GoWatch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  CI2GoWatch Extension
+//
+//  Created by Atsushi Nagase on 2018/06/23.
+//  Copyright © 2018 LittleApps Inc. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
