0641ea7da35ecc8b005cddf9ca75a5e2b792d966
Atsushi Nagase <a@ngs.io>
2018-06-29 17:52:24 +0900

    Update JS

--- CI2Go/Assets/login-github.com.js
+++ CI2Go/Assets/login-github.com.js
@@ -1,5 +1,5 @@
 ((login, password) => {
 window.document.querySelectorAll('[name="login"]')[0].value = login;
 window.document.querySelectorAll('[name="password"]')[0].value = password;
-return "window.document.querySelectorAll('form[action=\"/session\"]')[0].submit()";
+ return "(form => { form.submit(); return 'OK' })(window.document.querySelectorAll('form[action=\"/session\"]')[0])";
 })
