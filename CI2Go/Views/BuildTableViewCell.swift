5f1fd60c895063fb2d1651283097384db1ac49ef
Atsushi Nagase <a@ngs.io>
2018-07-03 23:59:55 +0900

    Separate layout cell

--- CI2Go/Views/BuildTableViewCell.swift
+++ CI2Go/Views/BuildTableViewCell.swift
@@ -50,26 +50,6 @@ class BuildTableViewCell: CustomTableViewCell {
         isHidden = true
     }
 
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        let scheme = ColorScheme.current
-        let foreground: UIColor
-        let background: UIColor
-        if #available(iOSApplicationExtension 10, *) {
-            foreground = .darkText
-            background = .lightText
-        } else {
-            foreground = scheme.foreground
-            background = scheme.background
-        }
-        tintColor = foreground
-        statusLabel.textColor = background
-        statusLabel.highlightedTextColor = background
-        statusLabel.backgroundColor = build?.status.color
-        vcsIconImageView.image = build?.project.vcs.icon
-        timeLabel.text = build?.timestamp?.timeAgoSinceNow
-    }
-
     static func height(for build: Build?) -> CGFloat {
         return build?.hasWorkflows == true ? 95 : 75
     }
