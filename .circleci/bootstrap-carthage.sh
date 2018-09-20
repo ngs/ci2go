460fd42e63c74801a8f06a1591029f2ee21c75fc
Atsushi Nagase <a@ngs.io>
2018-09-21 02:08:19 +0900

    Patch pbxproj to fix deployment target
    https://stackoverflow.com/a/52315766

--- .circleci/bootstrap-carthage.sh
+++ .circleci/bootstrap-carthage.sh
@@ -3,5 +3,9 @@
 set -eu
 
 if ! cmp -s Cartfile.resolved Carthage/Cartfile.resolved; then
-  carthage bootstrap && cp Cartfile.resolved Carthage/Cartfile.resolved
+  carthage bootstrap --no-build
+  # Patch pbxproj https://stackoverflow.com/a/52315766
+  find Carthage/Checkouts \( -name '*.pbxproj' -o -name '*.xcconfig' \) -print0 | xargs -0 sed -i "" "s/WATCHOS_DEPLOYMENT_TARGET = 2.0/WATCHOS_DEPLOYMENT_TARGET = 4.0/g"
+  carthage build --platform iOS,watchOS
+  cp Cartfile.resolved Carthage/Cartfile.resolved
 fi
