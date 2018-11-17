3cb88b6e982af2bec781be75b7f3807113d02712
Atsushi Nagase <a@ngs.io>
2018-11-18 03:15:15 +0900

    Update Carthage modules

--- .circleci/bootstrap-carthage.sh
+++ .circleci/bootstrap-carthage.sh
@@ -5,7 +5,7 @@ set -eu
 if ! cmp -s Cartfile.resolved Carthage/Cartfile.resolved; then
   carthage bootstrap --no-build
   # Patch pbxproj https://stackoverflow.com/a/52315766
-  find Carthage/Checkouts \( -name '*.pbxproj' -o -name '*.xcconfig' \) -print0 | xargs -0 sed -i "" "s/WATCHOS_DEPLOYMENT_TARGET = 2.0/WATCHOS_DEPLOYMENT_TARGET = 4.0/g"
+  find Carthage/Checkouts \( -name '*.pbxproj' -o -name '*.xcconfig' \) -print0 | xargs -0 sed -i "" "s/WATCHOS_DEPLOYMENT_TARGET = 2.0/WATCHOS_DEPLOYMENT_TARGET = 5.0/g"
   carthage build --platform iOS,watchOS
   cp Cartfile.resolved Carthage/Cartfile.resolved
 fi
