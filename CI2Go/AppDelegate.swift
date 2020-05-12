171ee0c3f4a285af630936a58bceed5d34d449b4
Atsushi Nagase <a@ngs.io>
2020-05-12 13:09:16 +0900

    Handle split view delegate method in scene delegate

--- CI2Go/AppDelegate.swift
+++ CI2Go/AppDelegate.swift
@@ -7,73 +7,14 @@
 //
 
 import UIKit
-import KeychainAccess
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
-    var window: UIWindow?
-
+class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-
-        splitViewController?.delegate = self
-        splitViewController?.preferredDisplayMode = .allVisible
-
         activateWCSession()
-        #if DEBUG
-        if CommandLine.arguments.contains("UITestingDarkModeEnabled") {
-            window?.overrideUserInterfaceStyle = .dark
-        }
-        #endif
-        window?.tintColor = .lightGray
         return true
-    }
-
-    func application(_ app: UIApplication,
-                     open url: URL,
-                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
-        guard
-            let splitVC = window?.rootViewController as? MainSplitViewController,
-            let viewController = splitVC.buildsViewController
-            else { return false }
-
-        if let build = Build(inAppURL: url) ?? Build(webURL: url) {
-            viewController.navigationController?.popToViewController(viewController, animated: false)
-            viewController.performSegue(withIdentifier: .showBuildDetail, sender: build)
-            return true
-        }
-        if url.host == inAppHost &&
-            url.pathComponents.count == 3 &&
-            url.pathComponents[1] == "token" {
-            let token = url.pathComponents[2]
-            viewController.logout(showSettings: false)
-            Keychain.shared.setAndTransfer(token: token)
-            viewController.presentedViewController?.dismiss(animated: false, completion: nil)
-            viewController.navigationController?.popToViewController(viewController, animated: false)
-            Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
-                viewController.loadBuilds()
-            }
-            return true
-        }
-        return false
-    }
-
-    // MARK: - Split View
-
-    var splitViewController: UISplitViewController? {
-        return window?.rootViewController as? UISplitViewController
-    }
-
-    func splitViewController(
-        _ splitViewController: UISplitViewController,
-        collapseSecondary secondaryViewController: UIViewController,
-        onto primaryViewController: UIViewController) -> Bool {
-        if let secondaryAsNavController = secondaryViewController as? UINavigationController {
-            let viewController = secondaryAsNavController.topViewController
-            return viewController is BuildLogViewController || viewController is TextViewController
-        }
-        return false
     }
 }
